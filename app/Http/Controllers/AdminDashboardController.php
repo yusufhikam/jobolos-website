28a59a65aa9d185c48d0c018eae25b1d63b0eba7
yusufhikam <hikamh2r@gmail.com>
2024-07-06 17:14:23 +0700

    add pendapatan total adminDashboard

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -19,8 +19,11 @@ class AdminDashboardController extends Controller
         $paymentRentalConfirm = RentalPayment::where('status_pembayaran', 'pending')->count();
         $newRentalBooking = Rental::where('status', 'pending')->count();
 
+        $pendapatanPhotoshoot = Payment::count('total_harga');
+        $pendpaatanRental = RentalPayment::count('total_harga');
 
-        return view('/admin_panel/adminDashboard', compact('newBooking', 'userRegistered', 'paymentConfirm', 'paymentRentalConfirm', 'newRentalBooking'));
+
+        return view('/admin_panel/adminDashboard', compact('newBooking', 'userRegistered', 'paymentConfirm', 'paymentRentalConfirm', 'newRentalBooking', 'pendpatanPhotoshoot', 'pendapatanRental'));
     }
 
     // METHOD UNTUK MENANDAI CALENDAR JIKA ADA PHOTOSHOOT BOOKING
--- resources/views/admin_panel/adminDashboard.blade.php
+++ resources/views/admin_panel/adminDashboard.blade.php
@@ -76,6 +76,36 @@
                                     class="fas fa-arrow-circle-right"></i></a>
                         </div>
                     </div>
+                    <div class="col-lg-4 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>{{ $userRegistered }}</h3>
+
+                                <p>Pendapatan Photoshoot</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="/admin_panel/adminManageUser" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-6">
+                        <!-- small box -->
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>{{ $userRegistered }}</h3>
+
+                                <p>Pendapatan Rental Kamera</p>
+                            </div>
+                            <div class="icon">
+                                <i class="ion ion-person-add"></i>
+                            </div>
+                            <a href="/admin_panel/adminManageUser" class="small-box-footer">More info <i
+                                    class="fas fa-arrow-circle-right"></i></a>
+                        </div>
+                    </div>
 
                     <div class="col-lg-6 col-6">
                         <!-- small box -->
